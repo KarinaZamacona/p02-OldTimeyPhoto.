d6f198621056bcd996599cc3211fcb36a6b80034
KarinaZamacona <azamacona@mail.csuchico.edu>
2017-10-02 16:50:09 -0700

    Outline for project 2

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include "bitmap.h"
+#include <vector>
+using namespace std;
 
+//determind char r,g,b;
+//r=red, g=green, b=blue;
+int main()
+{
+//ask the user for the file name of an image in the format Windows BMP
+//determine if the file is correct or if there was an error
+//count the "rows" or width and "columns" or height of the image
+//make each pixle into a vector with cooridates <r,g,b>
+//determine values of r,g, and b
+//convert the every pixle in the image from colors to a gray scale by taking the sum of the chars and dividing by the number of chars (r+g+b)/3
+//repeat this for each vector or pixle until entire image has been converted
+//save new image 
+return 0;
+}
