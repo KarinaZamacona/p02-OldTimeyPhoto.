08f381b36378e4e22faa3e471fbe87c9a015493d
KarinaZamacona <azamacona@mail.csuchico.edu>
2017-10-04 16:59:04 -0700

    trying to push back bitmap.h

--- main.cpp
+++ main.cpp
@@ -3,10 +3,14 @@
 #include <vector>
 using namespace std;
 
-//determind char r,g,b;
-//r=red, g=green, b=blue;
 int main()
 {
+//determind char r,g,b;
+//r=red, g=green, b=blue;
+Bitmap image;
+vector<vector<Pixle>>bmp;
+Pixel rgb;
+
 //ask the user for the file name of an image in the format Windows BMP
 //determine if the file is correct or if there was an error
 //count the "rows" or width and "columns" or height of the image
@@ -15,5 +19,6 @@ int main()
 //convert the every pixle in the image from colors to a gray scale by taking the sum of the chars and dividing by the number of chars (r+g+b)/3
 //repeat this for each vector or pixle until entire image has been converted
 //save new image 
+
 return 0;
 }
